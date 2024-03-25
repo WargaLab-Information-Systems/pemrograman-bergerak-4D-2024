647b510569e0c806baf97f06a46203521d34d6b0
Verolynz <220441100156@student.trunojoyo.ac.id>
2024-03-25 15:05:55 +0700

    220441100156_Mohammad Hilmi Bisri_Kak M Syahrul Abidin_Modul-1

---
+++ modul-1/220441100156_MHilmiBisri/Tugas Modul 1.kt
@@ -0,0 +1,18 @@
+class Kalculator {
+    fun Tambah(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun Kurang(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun Kali(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun Bagi(a: Int, b: Int): Int {
+        return a / b
+    }
+}
+
