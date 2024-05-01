6c35546697036598ae26bf532e00a8dfe6ac1b9c
hasnahinsita <hasnahinsita@gmail.com>
2024-05-01 19:58:13 +0700

    220441100144-HASNAH INSITA SAHLA-MUHAMMAD PUTRA SENJA KEIMANA-MODUL2

---
+++ modul-2/220441100144-HasnahInsitaSahla/tugasmodul2/app/src/main/java/com/example/tugasmodul2/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.tugasmodul2
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+    fun beranda(view: View?) {
+        val intent = Intent(this, beranda::class.java)
+        startActivity(intent)
+    }
+
+    private fun enableEdgeToEdge() {
+    }
+}
+
---
+++ modul-2/220441100144-HasnahInsitaSahla/tugasmodul2/app/src/main/java/com/example/tugasmodul2/beranda.kt
@@ -0,0 +1,26 @@
+package com.example.tugasmodul2
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class beranda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_beranda)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun profile(view: View?) {
+        val intent = Intent(this, profil::class.java)
+        startActivity(intent)
+    }
+}
