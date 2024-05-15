019d472c4c69269cc17185f79ef3336a2b9235de
Firdausi Putri Cahyani <firdausiputri14@gmail.com>
2024-05-15 17:15:51 +0700

    220441100133-FIRDAUSI PUTRI CAHYANI-M.PUTRA SENJA KEIMANA-MODUL3

---
+++ modul-3/220441100133-FirdausiPutriCahyani/Mod3-RecyclerView/app/src/main/java/com/vinz/latihanrecyclerview1/data/DataList.kt
@@ -0,0 +1,95 @@
+package com.vinz.latihanrecyclerview1.data
+
+import com.vinz.latihanrecyclerview1.R
+
+object DataList {
+    val playerDataValue = arrayListOf(
+        DataTempat(
+            "National Park Yosante",
+            "California",
+            R.drawable.gambar1,
+            R.drawable.lokasi
+        ),
+        DataTempat(
+            "Whitehaven Beach",
+            "Australia",
+            R.drawable.gambar2,
+            R.drawable.lokasi
+        )
+    )
+
+    val playerDataGridValue = arrayListOf(
+        HotelData(
+            "Ubud’s Hotel",
+            "Kediri Hotel adalah sebuah hotel mewah yang terletak di pusat kota yang ramai, menawarkan pengalaman menginap yang tak tertandingi bagi para tamu yang mencari keseimbangan sempurna antara kemewahan, kenyamanan, dan layanan berkualitas. Dengan desain interior yang elegan dan modern, serta fasilitas yang lengkap, Kediri Hotel menjadi pilihan utama bagi pelancong bisnis maupun wisatawan yang menginginkan akomodasi yang istimewa. Setiap kamar dilengkapi dengan fasilitas modern seperti WiFi gratis, TV layar datar, dan kamar mandi pribadi dengan perlengkapan mandi mewah. Selain itu, para tamu dapat menikmati berbagai pilihan kuliner lezat di restoran hotel yang terkenal dengan hidangan lokal dan internasional yang disajikan dengan cita rasa yang istimewa. Dengan pemandangan kota yang menakjubkan dari kamar-kamar yang luas, Kediri Hotel memberikan pengalaman menginap yang tak terlupakan bagi setiap tamu yang datang.",
+            R.drawable.hotel1,
+            R.drawable.org,
+            "50",
+            R.drawable.bintang,
+            "50"
+        ),
+        HotelData(
+            "Ubud’s Hotel",
+            "",
+            R.drawable.hotel2,
+            R.drawable.org,
+            "50",
+            R.drawable.bintang,
+            "50"
+        ),
+        HotelData(
+            "Ubud’s Hotel",
+            "",
+            R.drawable.hotel3,
+            R.drawable.org,
+            "50",
+            R.drawable.bintang,
+            "50"
+        ),
+        HotelData(
+            "Ubud’s Hotel",
+            "",
+            R.drawable.hotel4,
+            R.drawable.org,
+            "50",
+            R.drawable.bintang,
+            "50"
+        ),
+        HotelData(
+            "Ubud’s Hotel",
+            "",
+            R.drawable.hotel4,
+            R.drawable.org,
+            "50",
+            R.drawable.bintang,
+            "50"
+        ),
+        HotelData(
+            "Ubud’s Hotel",
+            "",
+            R.drawable.hotel4,
+            R.drawable.org,
+            "50",
+            R.drawable.bintang,
+            "50"
+        ),
+        HotelData(
+            "Ubud’s Hotel",
+            "",
+            R.drawable.hotel4,
+            R.drawable.org,
+            "50",
+            R.drawable.bintang,
+            "50"
+        ),
+        HotelData(
+            "Ubud’s Hotel",
+            "",
+            R.drawable.hotel4,
+            R.drawable.org,
+            "50",
+            R.drawable.bintang,
+            "50"
+        )
+    )
+}
