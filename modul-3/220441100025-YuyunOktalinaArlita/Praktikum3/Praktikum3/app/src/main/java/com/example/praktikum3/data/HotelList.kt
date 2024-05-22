d1dfd5e0d466744639b7d2bc2d4531b3813cbf96
yuyunoktalina <yuyunoktalina25@gmail.com>
2024-05-22 09:04:51 +0700

    220441100025_YUYUN OKTALINA_MODUL3_KAK ANGGA

---
+++ modul-3/220441100025-YuyunOktalinaArlita/Praktikum3/Praktikum3/app/src/main/java/com/example/praktikum3/data/HotelList.kt
@@ -0,0 +1,86 @@
+package com.example.praktikum3.data
+
+import com.example.praktikum3.R
+
+
+object HotelList {
+
+    val Data = arrayListOf(
+        HotelData(
+            "Taman Bromo",
+            "Tempat ini berada di ketinggian 2.770 meter di atas laut (mdpl). Untuk menuju lokasi ini, wisatawan bisa melalui tiga rute.",
+                   "Lumajang",
+                    R.drawable.gambar11
+        ),
+        HotelData(
+            "Danau Beratan",
+            "Danau Beratan, atau yang dikenal sebagai Danau Bratan merupakan wisata danau yang berada di kawasan Bedugul, Desa Candikuning, Kec. Baturiti, Tabanan, Bali. Karena berada di ketinggian 1.240 mdpl, tak heran jika suhu udara yang ada di sekitar danau sangat sejuk dan segar.",
+            "Denpasar",
+            R.drawable.gambar22
+        ),
+        HotelData(
+            "Raja Ampat",
+            "Kepulauan Raja Ampat adalah gugusan kepulauan yang berlokasi di barat bagian Semenanjung Kepala Burung (Vogelkoop) Pulau Papua. Secara administrasi, gugusan ini berada di bawah Kabupaten Raja Ampat dan Kota Sorong, Provinsi Papua Barat Daya",
+            "Jayapura",
+            R.drawable.gambar33
+        ),
+        HotelData(
+            "Candi Borobudur",
+            "Monumen ini terdiri atas enam teras berbentuk bujur sangkar yang di atasnya terdapat tiga pelataran melingkar, pada dindingnya dihiasi dengan 2.672 panel relief dan aslinya terdapat 504 arca Buddha..",
+            "Magelang",
+            R.drawable.gambar44
+        ),
+        HotelData(
+            "Danau Beratan",
+            "Danau Beratan, atau yang dikenal sebagai Danau Bratan merupakan wisata danau yang berada di kawasan Bedugul, Desa Candikuning, Kec. Baturiti, Tabanan, Bali. Karena berada di ketinggian 1.240 mdpl, tak heran jika suhu udara yang ada di sekitar danau sangat sejuk dan segar.",
+            "Denpasar",
+            R.drawable.gambar22
+        ),
+        HotelData(
+            "Candi Borobudur",
+            "Monumen ini terdiri atas enam teras berbentuk bujur sangkar yang di atasnya terdapat tiga pelataran melingkar, pada dindingnya dihiasi dengan 2.672 panel relief dan aslinya terdapat 504 arca Buddha..",
+            "Magelang",
+            R.drawable.gambar44
+        ),
+    )
+
+    val Grid = arrayListOf(
+        HotelData(
+            "Borobudur Jakarta",
+            "Hotel Borobudur Jakarta memiliki berbagai fasilitas terbaik untuk melengkapi kebutuhan anda. Salah satu keunggulan dari hotel berbintang 5 satu ini adalah aksesnya pada taman bermain dan taman rekreasi seluas 23 hektar yang cocok sebagai lokasi liburan keluarga yang nyaman. .",
+            "Jakarta",
+            R.drawable.hotel1
+        ),
+        HotelData(
+            "Manhattan Hotel",
+            "hotel bintang 5 yang berdiri di kawasan kuningan. Sesuai dengan namanya, hotel mewah satu ini memiliki desain eksterior dan interior mewah ala kota New York. Memadukan antara unsur klasik dengan minimalis modern yang memberikan nuansa elegan pada setiap sudut Manhattan Hotel. ",
+            "Bandung",
+            R.drawable.hotel2
+        ),
+        HotelData(
+            "Capung Se Bali",
+            "Chapung Se Bali Resort terletak di lereng bukit Ubud.  Dengan bangunan yang dominan terbuat dari kayu villa ini memberikan suasana back to nature yang mewah.  Chapung Se Bali Resort ini menyediakan villa dengan 1 kamar, 2 kamar, dan luxury residence with private pool.",
+            "Denpasar",
+            R.drawable.hotel3
+        ),
+        HotelData(
+            "Borobudur Jakarta",
+            "Hotel Borobudur Jakarta memiliki berbagai fasilitas terbaik untuk melengkapi kebutuhan anda. Salah satu keunggulan dari hotel berbintang 5 satu ini adalah aksesnya pada taman bermain dan taman rekreasi seluas 23 hektar yang cocok sebagai lokasi liburan keluarga yang nyaman. .",
+            "Jakarta",
+            R.drawable.hotel1
+        ),
+        HotelData(
+            "Capung Se Bali",
+            "Chapung Se Bali Resort terletak di lereng bukit Ubud.  Dengan bangunan yang dominan terbuat dari kayu villa ini memberikan suasana back to nature yang mewah.  Chapung Se Bali Resort ini menyediakan villa dengan 1 kamar, 2 kamar, dan luxury residence with private pool.",
+            "Denpasar",
+            R.drawable.hotel3
+        ),
+        HotelData(
+            "Manhattan Hotel",
+            "hotel bintang 5 yang berdiri di kawasan kuningan. Sesuai dengan namanya, hotel mewah satu ini memiliki desain eksterior dan interior mewah ala kota New York. Memadukan antara unsur klasik dengan minimalis modern yang memberikan nuansa elegan pada setiap sudut Manhattan Hotel. ",
+            "Bandung",
+            R.drawable.hotel2
+        ),
+    )
+
+}
