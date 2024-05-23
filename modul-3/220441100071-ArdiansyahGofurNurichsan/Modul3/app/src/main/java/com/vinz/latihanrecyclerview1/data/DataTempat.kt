b1048fbb8b1919fc46f8a94e7eed5ffe7dc8fff2
Ardiansyahygy27 <ardiansyahgofur@gmail.com>
2024-05-23 18:09:27 +0700

    220441100071-ArdiansyahGofur-AsprakKakTamam

---
+++ modul-3/220441100071-ArdiansyahGofurNurichsan/Modul3/app/src/main/java/com/vinz/latihanrecyclerview1/data/DataTempat.kt
@@ -0,0 +1,11 @@
+package com.vinz.latihanrecyclerview1.data
+
+// Seperti variabel namun lebih powerful, fungsinya untuk menyimpan data yang kita butuhkan
+data class DataTempat(
+    val name : String,
+    val nameLoc : String,
+    val image : Int,
+    val icon : Int
+)
+
+
