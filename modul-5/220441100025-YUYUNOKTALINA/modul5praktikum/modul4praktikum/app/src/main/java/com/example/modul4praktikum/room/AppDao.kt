06b6be4931ee68d2af7905f852e9b811b3b2bd0a
yuyunoktalina <yuyunoktalina25@gmail.com>
2024-06-09 22:54:43 +0700

    220441100025_YUYUN OKTALINA_MODUL4&5_KAK ANGGA

---
+++ modul-5/220441100025-YUYUNOKTALINA/modul5praktikum/modul4praktikum/app/src/main/java/com/example/modul4praktikum/room/AppDao.kt
@@ -0,0 +1,24 @@
+package com.example.modul4praktikum.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+//membuat DAO unutuk bisa mengakses databse agar bisa menghpuys memasukan data dll)
+@Dao
+interface AppDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertPost(post:PostDatabase)
+    @Query("SELECT * from postdatabase  ORDER BY id DESC")
+    fun getAllPost(): LiveData<List<PostDatabase>>
+    @Delete
+    fun deletePost(post: PostDatabase)
+    @Update
+    fun updatePost(post: PostDatabase)
+
+
+}
